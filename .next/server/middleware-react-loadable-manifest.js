68b7dc27903b19dac87546c1b7abbe40e18b9480
Jiun Haw Chu <jiunhawc@usc.edu>
2025-05-31 01:50:19 -0700

    Remove .next directory from tracking

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components/PackingWeightChart.tsx -> recharts\":{\"id\":\"components/PackingWeightChart.tsx -> recharts\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_recharts_es6_index_js.js\"]}}"
